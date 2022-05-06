21225802c0298f02d6d849a7017de321ef7479aa
James Smith <jams.websmith@gmail.com>
2022-05-06 18:18:53 +0100

    use Heading component in Home->Services

--- site/src/Views/Client/Home/Services.js
+++ site/src/Views/Client/Home/Services.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
+import Heading from '../../../Components/Headings/Heading.js';
 import {
   Wrapper,
-  Title,
   IntroText,
   ServicesList,
   ListItem,
@@ -14,7 +14,7 @@ import {
 const Services = () => {
   return (
     <Wrapper>
-      <Title>Our Services</Title>
+      <Heading text='Our Services' />
       <IntroText>All of our work is carried out in our small but well equipped workshop in Wales, UK.</IntroText>
       <IntroText>As well as our stock items we also offer a range of high quality bespoke services.  Please get in touch to enquire about any of these.</IntroText>
       <ServicesList>
