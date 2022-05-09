4b31bfe538c65604d9314fbca62bdcfded937f9e
James Smith <jams.websmith@gmail.com>
2022-05-09 13:43:44 +0100

    fix getEnvVar helper error

--- site/src/helpers/env.js
+++ site/src/helpers/env.js
@@ -1,5 +1,5 @@
 export const getEnvVar = (variable) => {
-  if (process.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     return process.env[`REACT_APP_LIVE_${variable}`];
   } else {
     return process.env[`REACT_APP_LOCAL_${variable}`];
